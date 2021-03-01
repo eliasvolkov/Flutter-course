30519b1ac35baf627215afb82e07fa5e0c7fb5d6
ilia volkov <Ilya.Volkov@noveogroup.com>
2021-03-01 16:26:27 +0300

    [IV] - reset master

--- lib/app.dart
+++ lib/app.dart
@@ -8,46 +8,17 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  int _questionIndex = 0;
-
-  final questions = [
-    {
-      'question': 'What\'s your name?',
-      'answers': ['First', 'Second', 'Third']
-    },
-    {
-      'question': 'Do you like futter?',
-      'answers': ['Yes', 'No', 'I don\'t know']
-    },
-  ];
-
-  void _answerQuestion() {
-    setState(() {
-      _questionIndex++;
-    });
-  }
-
-  void _onGoBack() {
-    setState(() {
-      _questionIndex = 0;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       home: Scaffold(
-        appBar: AppBar(
-          title: Text("Quize App"),
-        ),
-        body: _questionIndex < questions.length
-            ? Quiz(
-                questions: questions,
-                questionIndex: _questionIndex,
-                answerQuestion: _answerQuestion)
-            : Finish(goBack: _onGoBack),
-      ),
+          appBar: AppBar(
+            title: Text("Flutter App"),
+          ),
+          body: Center(
+            child: Text("Flutter Application"),
+          )),
     );
   }
 }
