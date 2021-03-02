893a340537f38c2acb5575756203be296a0d81b2
ilia volkov <Ilya.Volkov@noveogroup.com>
2021-03-02 16:04:45 +0300

    [IV] - added modal

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:quize_app/models/transaction.dart';
+import 'package:quize_app/widgets/new_transaction.dart';
+import 'package:quize_app/widgets/transaction_list.dart';
 import 'package:quize_app/widgets/user_transactions.dart';
 
 class App extends StatefulWidget {
@@ -7,18 +10,43 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  List<Transaction> transactions = [
+    Transaction(id: '1', title: "Milk", amount: 3, date: DateTime.now()),
+    Transaction(id: '2', title: "Bread", amount: 2.5, date: DateTime.now()),
+  ];
+
+  void _addTransaction(String title, amount) {
+    final newTransaction = Transaction(
+        id: DateTime.now().toString(),
+        title: title,
+        amount: double.parse(amount),
+        date: DateTime.now());
+
+    setState(() {
+      transactions.add(newTransaction);
+    });
+  }
+
+  void _showTransactionModal(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return NewTransaction(_addTransaction);
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter App"),
-        ),
-        body: SingleChildScrollView(
-          child: Column(
-            children: [UserTransaction()],
-          ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter App"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _showTransactionModal(context),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [TransactionList(transactions)],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 import 'app.dart';
 
-void main() => runApp(App());
+void main() => runApp(MyApp());
 
-
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: App(),
+    );
+  }
+}
